c23d65aa2cd5aa3a242aee0b77220fe2facd204a
Averyjreed <avery-reed@uiowa.edu>
2017-05-02 19:09:33 -0500

    adding the project to gitHub

---
+++ src/finalproject/FinalProject.java
@@ -0,0 +1,17 @@
+package finalproject;
+
+/**
+ *
+ * @author mattbentley
+ */
+public class FinalProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new HomePage().setVisible(true);
+        
+    }
+    
+}
